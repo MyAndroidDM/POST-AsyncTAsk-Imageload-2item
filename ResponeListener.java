710fe5d76bc7e7e4b7edf2b8e8e4b7df8e5d4709
MyAndroidDM <3056481629@qq.com>
2017-04-28 17:04:40 +0800

    interface接口
    interface接口

---
+++ ResponeListener.java
@@ -0,0 +1,10 @@
+package bawei.com.homework20170428.listener;
+
+/**
+ * Created by huanhuan on 2017/4/28.
+ */
+
+public interface ResponeListener {
+    public void success(String string);
+    public void onFial();
+}
